0a7b8331a13113272b1376471a23ffdc41b97bc5
Victor Bernardino <victorbernardino@gmail.com>
2015-08-16 23:32:18 -0300

    Testing microsoft tool called "Code". Removing unused line

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ app.use(express.static(__dirname + '/public'));
  
 routes.constructor(app);
 
-var server = app.listen(config.port, function(){
-	var host = server.address().address;
+var server = app.listen(config.port, function(){	
  	var port = server.address().port;
-
+	 
  	console.log('Server started at http://%s:%s', config.ip, port);
 });
