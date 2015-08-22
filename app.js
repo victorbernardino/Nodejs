e3c893cd2221a627b5817099c62fa865a3336508
Victor Bernardino <victorbernardino@gmail.com>
2015-08-21 23:17:15 -0300

    Formating files with vs code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express'),
- 	config = require('./config/config.js'),
- 	routes = require('./routes.js');
+	config = require('./config/config.js'),
+	routes = require('./routes.js');
 
 var app = express();
 
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/view'));
 app.use(express.static(__dirname + '/public'));
- 
+
 routes.constructor(app);
 
-var server = app.listen(config.port, function(){	
- 	var port = server.address().port;
-	 
- 	console.log('Server started at http://%s:%s', config.ip, port);
+var server = app.listen(config.port, function () {
+	var port = server.address().port;
+
+	console.log('Server started at http://%s:%s', config.ip, port);
 });
--- routes.js
+++ routes.js
@@ -1,28 +1,28 @@
-exports.constructor = function(app){
+exports.constructor = function (app) {
 	app.get('/', homePage);
 	app.get('/2', secondPage);
 	app.get('/about', about);
-	app.get('/eitcha', eitcha);	
+	app.get('/eitcha', eitcha);
 	app.use(error);
 };
 
-function homePage(req, res){
-	res.render(__dirname+ '/view/index.html');
+function homePage(req, res) {
+	res.render(__dirname + '/view/index.html');
 }
 
-function secondPage(req, res){	
-	res.sendFile(__dirname +'/view/secondPage.html');
+function secondPage(req, res) {
+	res.sendFile(__dirname + '/view/secondPage.html');
 }
 
-function about(req, res){
+function about(req, res) {
 	res.sendFile(__dirname + '/view/about.html');
 }
 
-function eitcha(req, res){
+function eitcha(req, res) {
 	res.send('Eitcha!');
 }
 
-function error(err, req, res, next){
+function error(err, req, res, next) {
 	console.error(err.stack);
 	res.status(404).send('Sorry for the inconvenience but an error just happened!');
 }
